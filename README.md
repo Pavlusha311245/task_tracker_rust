fd19a49ae259594cb92db5e5b8e04c31922d4833
Pavel <zavadskiy.pavel2002@outlook.com>
2025-02-05 21:18:06 +0100

    Add task management functionality to CLI app
    Implemented CRUD operations for tasks, including adding, updating, removing, and changing task statuses. Integrated serialization with `serde` and `serde_json` for persistent storage in a JSON file. Added CLI commands for listing tasks, including filtering by status.

---
+++ README.md
@@ -0,0 +1,54 @@
+# Rust task tracker
+
+A simple task tracker CLI application
+
+## Installation
+
+1. Clone the repository
+
+ ```bash
+ git clone <github-repo-url>
+ ```
+
+2. Build the application
+
+```bash
+cargo build --release
+``` 
+
+## Usage
+
+### Adding a new task
+
+ ```bash
+ task-cli add "Buy groceries"
+ # Output: Task added successfully (ID: 1)
+ ```
+
+### Updating and deleting tasks
+
+ ```bash
+ task-cli update 1 "Buy groceries and cook dinner"
+ task-cli delete 1
+ ```
+
+### Marking a task as in progress or done
+
+ ```bash
+ task-cli mark-in-progress 1
+ task-cli mark-done 1
+ ```
+
+### Listing all tasks
+
+ ```bash
+ task-cli list
+ ```
+
+### Listing tasks by status
+
+ ```bash
+ task-cli list done
+ task-cli list todo
+ task-cli list in-progress
+ ```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
 
+#[derive(Serialize, Deserialize, Debug)]
 struct Task {
     id: i32,
     description: String,
@@ -8,7 +13,7 @@ struct Task {
     updated_at: String,
 }
 
-fn main() {
+fn main() -> () {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
@@ -36,48 +41,174 @@ fn main() {
         add_task(String::from(args[2].as_str()));
     }
 
-    // println!("{:?}", args);
+    if args[1].as_str() == String::from("remove") {
+        if args.len() < 3 {
+            panic!("Missing id");
+        }
 
-    // TODO: crud
-    // TODO: Mark a task as in progress or done
-    // TODO: List all tasks
-    // TODO: List all tasks that are done
-    // TODO: List all tasks that are not done
-    // TODO: List all tasks that are in progress
+        remove_task(args[2].parse::<i32>().unwrap());
+    }
 
-    // task-cli add/delete/update <id> <options
-    // task-cli mark-in-progress/done
-    // task-cli list
-    // TODO: task-cli list done
-    // TODO: task-cli list todo
-    // TODO: task-cli list in-progress
+    if args[1].as_str() == String::from("update") {
+        if args.len() < 4 {
+            panic!("Missing id");
+        }
+
+        update_task_description(
+            args[2].parse::<i32>().unwrap(),
+            String::from(args[3].as_str()),
+        );
+    }
+    
+    let commands = vec!["mark-in-progress", "mark-done"];
+    if commands.contains(&args[1].as_str()) {
+        if args.len() < 3 {
+            panic!("Missing id");
+        }
+        
+        let status = match args[1].as_str() {
+            "mark-in-progress" => "in-progress",
+            "mark-done" => "done",
+            _ => panic!("Invalid command")
+        };
+        
+        change_task_status(args[2].parse::<i32>().unwrap(), status.to_string());
+    }
 }
 
-fn mark_in_progress(id: i32) -> () {
-    //
+fn change_task_status(id: i32, status: String) -> () {
+    let mut tasks: Vec<Task> = get_tasks();
+
+    let task_position = tasks.iter().position(|task| task.id == id);
+
+    if task_position.is_none() {
+        println!("Task not found");
+        return;
+    }
+
+    if tasks[task_position.unwrap()].status == "done" {
+        println!("Task is already done");
+        return;
+    }
+
+    tasks[task_position.unwrap()].status = status;
+    tasks[task_position.unwrap()].updated_at = chrono::offset::Utc::now().to_rfc3339();
+
+    write_tasks(tasks).ok();
+
+    let updated_task = get_tasks().into_iter().find(|task| task.id == id).unwrap();
+
+    println!(
+        "Task marked as {} successfully (ID: {})",
+        updated_task.status, id
+    );
 }
 
-fn mark_done(id: i32) -> () {
-    //
-}
+fn add_task(description: String) {
+    let mut tasks: Vec<Task> = get_tasks();
 
-fn add_task(description: String) -> () {
-    // Task {
-    //     name,
-    //     status: String::from("todo"),
-    // };
+    let new_id = if let Some(max_id) = tasks.iter().map(|task| task.id).max() {
+        max_id + 1
+    } else {
+        1
+    };
+
+    let date: String = chrono::offset::Utc::now().to_rfc3339();
+
+    let task = Task {
+        id: new_id,
+        description,
+        status: String::from("todo"),
+        created_at: date.clone(),
+        updated_at: date.clone(),
+    };
+    
+    tasks.push(task);
+    
+    write_tasks(tasks).ok();
+    
+    let added_task = get_tasks().into_iter().find(|task| task.id == new_id).unwrap();
+
+    println!("Task added successfully (ID: {})", added_task.id);
 }
 
 fn remove_task(id: i32) -> () {
-    //
+    let tasks: Vec<Task> = get_tasks();
+
+    let task_position = tasks.iter().position(|task| task.id == id);
+
+    if task_position.is_none() {
+        println!("Task not found");
+        return;
+    }
+
+    write_tasks(tasks.into_iter().filter(|task| task.id != id).collect()).ok();
+
+    println!("Task removed successfully (ID: {})", id);
 }
 
-fn update_task(id: i32, description: String) -> () {
-    //
+fn update_task_description(id: i32, description: String) -> () {
+    let mut tasks: Vec<Task> = get_tasks();
+
+    let task_position = tasks.iter().position(|task| task.id == id);
+
+    if task_position.is_none() {
+        println!("Task not found");
+        return;
+    }
+
+    tasks[task_position.unwrap()].description = description;
+    tasks[task_position.unwrap()].updated_at = chrono::offset::Utc::now().to_rfc3339();
+
+    write_tasks(tasks).ok();
+
+    println!("Task updated successfully (ID: {})", id);
 }
 
-fn list_tasks(status: Option<String>) -> () {
-    let vector: Vec<Task> = Vec::new();
+fn list_tasks(status: Option<String>) {
+    let tasks = get_tasks();
 
-    println!("List of tasks with status: {:?}", status);
+    let filtered_tasks = match status {
+        Some(status) => tasks
+            .into_iter()
+            .filter(|task| task.status == status)
+            .collect(),
+        None => tasks,
+    };
+
+    for task in filtered_tasks {
+        println!(
+            "ID: {},\nDescription: {},\nStatus: {},\nCreated At: {},\nUpdated At: {}\n",
+            task.id, task.description, task.status, task.created_at, task.updated_at
+        );
+    }
+}
+
+fn get_tasks() -> Vec<Task> {
+    let is_file_exists = Path::exists("tasks.json".as_ref());
+
+    if !is_file_exists {
+        File::create("tasks.json").unwrap();
+    }
+
+    let mut file: File = File::open("tasks.json").unwrap();
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content).unwrap();
+
+    let tasks: Vec<Task> = serde_json::from_str(&file_content).unwrap_or_else(|_| Vec::new());
+
+    tasks
+}
+
+fn write_tasks(tasks: Vec<Task>) -> Result<(), std::io::Error> {
+    let json_string = serde_json::to_string_pretty(&tasks)?;
+
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("tasks.json")?;
+
+    file.write_all(json_string.as_bytes())?;
+    Ok(())
 }
